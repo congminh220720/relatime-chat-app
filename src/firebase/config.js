65dc079612b57b51c6219238bed052bfab262e78
congminh220720 <ncongminh91@gmail.com>
2022-02-11 23:21:31 +0700

    comment local emulators

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -19,12 +19,12 @@ firebase.analytics();
 const auth = firebase.auth();
 const db = firebase.firestore();
 
-auth.useEmulator("http://localhost:9099");
+// auth.useEmulator("http://localhost:9099");
 
-if (window.location.hostname === "localhost") {
-  db.useEmulator("localhost", "8080");
-  auth.useEmulator("http://localhost:9099");
-}
+// if (window.location.hostname === "localhost") {
+//   db.useEmulator("localhost", "8080");
+//   auth.useEmulator("http://localhost:9099");
+// }
 
 export { db, auth };
 export default firebase;
